371817866c399449542247e013518d7980a5cb62
Paul Wooten <paul.wooten@hyland.com>
2024-11-15 13:42:37 -0500

    feat: hello world w/ express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import express from "npm:express";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const port = 8000;
+const app = express();
+import { Request, Response } from "npm:express";
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+app.listen(port);
+console.log(`Server running on http://localhost:${port}`);
