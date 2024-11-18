e4719d196b184320bd001be78ae344ecc8741cfa
Paul Wooten <paul.wooten@hyland.com>
2024-11-18 10:50:05 -0500

    refactor: fix broken tests

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express from "npm:express";
+import { createServer } from "node:http";
 import { initialize } from "./express-server.ts";
 
 const port = 8000;
 const app: express.Express = express();
-initialize(app, port, console);
+const server = createServer(app);
+initialize(app, server, port, console);
