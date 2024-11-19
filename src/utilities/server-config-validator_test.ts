02bd3cddbb884eab676629b0f893705ebd5969e8
Paul Wooten <paul.wooten@hyland.com>
2024-11-19 13:53:52 -0500

    feat: hostname tests

--- src/utilities/server-config-validator_test.ts
+++ src/utilities/server-config-validator_test.ts
@@ -1,7 +1,9 @@
 import { assertEquals } from "@std/assert";
 import { ErrorMessages } from "../constants/messages.ts";
+import { ServerConfig } from "../../server-config.ts";
 import { validateServerConfig } from "./server-config-validator.ts";
 
+//#region Port Tests
 Deno.test(function validateServerConfig_portTooLow() {
     try {
         const badConfig = { hostName: 'localhost', port: 0 };
@@ -18,3 +20,21 @@ Deno.test(function validateServerConfig_portTooHigh() {
         assertEquals(e.message, ErrorMessages.InvalidPortSpecified);
     }
 });
+//#endregion
+//#region Hostname Tests
+Deno.test(function validateServerConfig_noConfig() {
+    try {
+        validateServerConfig(undefined as unknown as ServerConfig );
+    } catch (e) {
+        assertEquals(e.message, ErrorMessages.NoServerConfigSpecified);
+    }
+});
+Deno.test(function validateServerConfig_invalidHostname() {
+    try {
+        const badConfig = { hostName: '', port: 8000 };
+        validateServerConfig(badConfig );
+    } catch (e) {
+        assertEquals(e.message, ErrorMessages.InvalidHostnameSpecified);
+    }
+});
+//#endregion
