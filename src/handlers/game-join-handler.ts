b197efed674b30d586d12dc8f0e3a6207294e08a
Paul Wooten <paul.wooten@hyland.com>
2024-11-20 09:33:24 -0500

    refactor: constant

--- src/handlers/game-join-handler.ts
+++ src/handlers/game-join-handler.ts
@@ -1,8 +1,9 @@
+import { SocketConstants } from "../constants/socket-constants.ts";
 import { BaseHandler } from "./base-handler.ts";
 
 export class GameJoinHandler extends BaseHandler {
     public handle(gameId: string): void {
         console.log(`Joining game with id: ${gameId}`);
-        this.socket.emit("join-game", gameId);
+        this.socket.emit(SocketConstants.JoinGame, gameId);
     }
 }
