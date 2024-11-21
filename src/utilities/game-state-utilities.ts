e5731c649a7116c79506d649e3b5cd9242b2c316
Paul Wooten <paul.wooten@hyland.com>
2024-11-21 16:12:46 -0500

    feat: additional themes wip

--- src/utilities/game-state-utilities.ts
+++ src/utilities/game-state-utilities.ts
@@ -5,17 +5,25 @@ export const themes = {
     burgerVsPizza: 'burger-vs-pizza',
     classic: 'classic',
     clownVsVomit: 'clown-vs-vomit',
-    dizzyVsMindBlown: 'dizzy-vs-mind-blown'
+    devilVsAngel: 'devil-vs-angel',
+    dizzyVsMindBlown: 'dizzy-vs-mind-blown',
+    dogVsCat: 'dog-vs-cat',
+    guitarVsMic: 'guitar-vs-mic',
+    meatVsSalad: 'meat-vs-salad',
 };
 const defaultMarks = ['X', 'O'];
 const themeMarks = new Map([
     [themes.burgerVsPizza, ['&#127828;', '&#127829;']],
     [themes.classic, defaultMarks],
     [themes.clownVsVomit, ['&#129313;', '&#129326;']],
-    [themes.dizzyVsMindBlown, ['&#128565;', '&#129327;']]
+    [themes.devilVsAngel, ['&#128520;', '&#128519;']],
+    [themes.dizzyVsMindBlown, ['&#128565;', '&#129327;']],
+    [themes.dogVsCat, ['&#128021;', '&#128008;']],
+    [themes.guitarVsMic, ['&#127928;', '&#127908;']],
+    [themes.meatVsSalad, ['&#129385;', '&#129367;']],
 ]);
 export const getUserMark = (game: GameState, user: string): string => {
-    const marks = themeMarks.get(themes.burgerVsPizza) || defaultMarks;
+    const marks = themeMarks.get(themes.meatVsSalad) || defaultMarks;
     const [x, o] = marks;
     return game.host === user ? x: o;
 
